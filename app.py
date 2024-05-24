db1fda5fa984c6c009e898f652faf2576048ec85
Gloireski <belemgloirebekoutou8@gmail.com>
2024-05-24 16:41:28 +0100

    set register route

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
-from flask import render_template, request
-app = Flask(__name__) 
- 
-@app.route('/') 
-def home(): 
+from flask import render_template, request, Blueprint
+
+app = Flask(__name__)
+
+symptom_reader_blueprint = Blueprint('s_reader', __name__)
+app.register_blueprint(symptom_reader_blueprint)
+
+
+@app.route('/')
+def home():
     return render_template("index.html")
- 
-if __name__ == '__main__': 
+
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
