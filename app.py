11843b26b12c781a8ab1f0cba1ba44f73f01ca6e
Amanda Mabunda <amandamabu14@gmail.com>
2024-05-27 11:59:15 -0700

    upadint app file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template, request, Blueprint
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -7,6 +8,8 @@ symptom_reader_blueprint = Blueprint('s_reader', __name__)
 app.register_blueprint(symptom_reader_blueprint)
 
 
+# config SQLite
+# still gonna write code, we need to decide if we creating a database or using an api
 @app.route('/')
 def home():
     return render_template("index.html")
@@ -15,6 +18,20 @@ def home():
 @app.route('/register')
 def register():
     return render_template("register.html")
+
+@app.route('/form')
+def form():
+    return render_template("user_form.html")
+
+
+@app.route('/general_form')
+def general_form():
+    return render_template("general_form.html")
+
+
+@app.route('/sidebar')
+def sidebar():
+    return render_template("sidebar.html")
 
 
 if __name__ == '__main__':
