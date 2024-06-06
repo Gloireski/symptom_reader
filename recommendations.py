6ac9637269a356df5a426f5ead5dddbfe90dda28
Amanda Mabunda <amandamabu14@gmail.com>
2024-06-06 08:34:10 -0700

    recommendations file

---
+++ recommendations.py
@@ -0,0 +1,90 @@
+#recommendations for some disease predictions
+
+recommendations = {
+    'Arthritis': {
+        'home_remedies': [
+            'https://www.medicalnewstoday.com/articles/324446',
+            'https://www.motrin.com/adults/arthritis/tips-remedies',
+            'https://www.verywellhealth.com/natural-remedies-for-arthritis-in-hands-5095949'
+        ],
+        'otc_medications': [
+            'https://www.mayoclinic.org/diseases-conditions/arthritis/diagnosis-treatment/drc-20350777',
+            'https://www.healthline.com/health/osteoarthritis/over-counter-relief',
+            'https://www.webmd.com/osteoarthritis/oa-pain-relief-risks-benefits'
+        ]
+    },
+    'Hyperthyroidism': {
+        'home_remedies': [
+            'https://drruscio.com/natural-remedies-for-hyperthyroidism/',
+            'https://www.healthline.com/health/hyperthyroidism-natural-treatment'
+        ],
+        'otc_medications': [
+            'https://www.drugs.com/condition/hyperthyroidism.html'
+        ]
+    },
+    'Hypothyroidism': {
+        'home_remedies': [
+            'https://www.medicalnewstoday.com/articles/remedies-for-hypothyroidism',
+            'https://www.healthline.com/health/hypothyroidism/five-natural-remedies-for-hypothyroidism'
+        ],
+        'otc_medications': []
+    },
+    'Chicken pox': {
+        'home_remedies': [
+            'https://www.everydayhealth.com/chickenpox/treatment-home-remedies/',
+            'https://www.medicalnewstoday.com/articles/326051#home-remedies'
+            'https://www.healthline.com/health/home-remedies-for-chickenpox#calamine-lotion',
+            'https://www.webmd.com/children/what-is-chickenpox'
+        ],
+        'otc_medications': [
+            'https://www.verywellhealth.com/chicken-pox-treatments-1068747',
+            'https://www.medicalnewstoday.com/articles/326051',
+            'https://www.everydayhealth.com/chickenpox/treatment-home-remedies/',
+            'https://www.cdc.gov/chickenpox/treatment/index.html'
+        ]
+    },
+    'Malaria': {
+        'home_remedies': [
+            'https://www.onlymyhealth.com/here-are-6-effective-home-remedies-and-prevention-tips-for-malaria-1616654099',
+            'https://www.ndtv.com/health/world-malaria-day-2018-9-most-effective-home-remedies-for-malaria-1842256',
+            'https://pharmeasy.in/blog/home-remedies-for-malaria-by-dr-siddharth-gupta/'
+        ],
+        'otc_medications': [
+            'https://www.metropolisindia.com/blog/preventive-healthcare/malaria-needs-your-attention-know-about-its-symptoms-causes-and-treatment'
+        ]
+    },
+    'Hypertension': {
+        'home_remedies': [
+            'https://www.medicinenet.com/how_to_lower_my_blood_pressure_immediately/article.htm',
+            'https://www.healthline.com/health/high-blood-pressure-home-remedies',
+            'https://www.tuasaude.com/en/natural-remedies-for-high-blood-pressure/'
+        ],
+        'otc_medications': [
+            'https://myhealth.alberta.ca/Health/pages/conditions.aspx?hwid=abq1040',
+            'https://share.upmc.com/2015/06/high-blood-pressure-over-the-counter-medicine-risks/'
+        ]
+    },
+    'Fungal Infection': {
+        'home_remedies': [
+            'https://redcliffelabs.com/myhealth/health/simple-home-remedies-for-fungal-infections/',
+            'https://pharmeasy.in/blog/try-these-simple-home-remedies-for-fungal-infections/'
+        ],
+        'otc_medications': [
+            'https://www.nhs.uk/conditions/antifungal-medicines/',
+            'https://www.healthdirect.gov.au/antifungal-medicines',
+            'https://www2.hse.ie/conditions/antifungal-medicines/',
+            'https://www.medicalnewstoday.com/articles/antifungal#types'
+        ]
+    },
+    'Common cold': {
+        'home_remedies': [
+            'https://rightasrain.uwmedicine.org/well/health/home-remedies-for-cold',
+            'https://www.healthline.com/health/cold-flu/home-remedies',
+            'https://www.medicalnewstoday.com/articles/324607'
+        ],
+        'otc_medications': [
+            'https://www.healthline.com/health/common-cold-drugs#cough-suppressants',
+            'https://www.verywellhealth.com/over-the-counter-cold-and-flu-medications-770618'
+        ]
+    }
+}
